e413ef4411891aec3d52c7d3fcd0e3cd87f0a1af
jonatasfarias <jotaflight@gmail.com>
2024-09-21 20:23:38 +0200

    testeok

--- conf_teste.py
+++ conf_teste.py
@@ -1,12 +1,13 @@
 from datetime import time
-import pytest
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+class test_1:
+    url_demo='https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+    driver = webdriver.Chrome()  # chamo o browser numa variável...o Driver é o controlador do Selenium
+    driver.get(url_demo)
+    driver.find_element(By.CSS_SELECTOR,'[name="username"]').send_keys('Admin')
 
-url_demo='https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-driver = webdriver.Chrome()  # chamo o browser numa variável...o Driver é o controlador do Selenium
-driver.get(url_demo)
-time.sleep(3)
 
 
 
