d7e723e7852dbae6b721be55e0813b5bae25d135
jonatasfarias <jotaflight@gmail.com>
2024-09-21 20:27:05 +0200

    testeok

--- conf_teste.py
+++ conf_teste.py
@@ -1,12 +1,13 @@
 from datetime import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 class test_1:
     url_demo='https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
     driver = webdriver.Chrome()  # chamo o browser numa variável...o Driver é o controlador do Selenium
     driver.get(url_demo)
-    driver.find_element(By.CSS_SELECTOR,'[name="username"]').send_keys('Admin')
+    time.sleep(3)
+    driver.find_element(By.CSS_SELECTOR,'[placeholder="Username"]').send_keys('Admin')
+
 
 
 
