600a8f866c92745a122b40fe5706ddfc96247992
jonatasfarias <jotaflight@gmail.com>
2024-09-23 21:52:46 +0200

    testeok

--- ConfTeste.py
+++ ConfTeste.py
@@ -10,9 +10,8 @@ class test_1:
         driver.find_element(By.CSS_SELECTOR, '[placeholder="Username"]').send_keys('Admin')
         driver.find_element(By.CSS_SELECTOR, '[placeholder="Password"]').send_keys('admin123')
         driver.find_element(By.CSS_SELECTOR,'#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button').click()
-        assert current_url==url_demo,'URL válida'
+        assert current_url=='https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index','URL válida'
         time.sleep(2)
-
         driver.quit()
         #consegui fazer o LOGIN
 
