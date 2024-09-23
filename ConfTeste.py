22eac7049f2500345bb22d6ef9696d1b926694b3
jonatasfarias <jotaflight@gmail.com>
2024-09-23 22:15:03 +0200

    testeok

--- ConfTeste.py
+++ ConfTeste.py
@@ -2,7 +2,7 @@ from driver import current_url
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-class test_1:
+class test:
         url_demo = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
         driver=webdriver.Chrome()
         driver.get(url_demo)
@@ -11,9 +11,8 @@ class test_1:
         driver.find_element(By.CSS_SELECTOR, '[placeholder="Username"]').send_keys('Admin')
         driver.find_element(By.CSS_SELECTOR, '[placeholder="Password"]').send_keys('admin123')
         driver.find_element(By.CSS_SELECTOR,'#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button').click()
-
-        assert current_url=='https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index','URL válida'
-        time.sleep(2)
+        time.sleep(5)
+        assert driver.current_url=='https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index',"URL INválida"
         driver.quit()
         #consegui fazer o LOGIN
 
