ce9f6263448ad054659a0c254daf6e29d266b7c0
jonatasfarias <jotaflight@gmail.com>
2024-09-23 21:46:50 +0200

    testeok

--- ConfTeste.py
+++ ConfTeste.py
@@ -6,8 +6,6 @@ from driver import current_url
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 class test_1:
-    @pytest.fixture
-    def open_browser(self):
         url_demo='https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
         driver = webdriver.Chrome()  # chamo o browser numa variável...o Driver é o controlador do Selenium
         driver.get(url_demo)
