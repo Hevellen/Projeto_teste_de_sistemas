d1f3230ab08b98152abd3f15bdda8d95c0f0c09b
jonatasfarias <jotaflight@gmail.com>
2024-09-23 22:46:47 +0200

    testeok

--- ConfTeste.py
+++ ConfTeste.py
@@ -2,14 +2,14 @@ import pytest
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-class test_1:
+class Test1:
         @pytest.fixture
         def open_browser(self):
                 url_demo = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-                self.driver = webdriver.Chrome()
+                self.driver=webdriver.Chrome()
                 self.driver.get(url_demo)
                 time.sleep(2)
-        def login(self,open_browser):
+        def test_login_button(self,open_browser):
                 self.driver.find_element(By.CSS_SELECTOR, '[placeholder="Username"]').send_keys('Admin')
                 self.driver.find_element(By.CSS_SELECTOR, '[placeholder="Password"]').send_keys('admin123')
                 self.driver.find_element(By.CSS_SELECTOR,'#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button').click()
