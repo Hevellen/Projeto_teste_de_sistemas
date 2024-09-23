43fc345f5fc07b4e04a6651bf6746b266a733afd
jonatasfarias <jotaflight@gmail.com>
2024-09-23 21:48:07 +0200

    testeok

--- ConfTeste.py
+++ ConfTeste.py
@@ -1,7 +1,4 @@
 import time
-from threading import current_thread
-
-import pytest
 from driver import current_url
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,7 +7,6 @@ class test_1:
         driver = webdriver.Chrome()  # chamo o browser numa variável...o Driver é o controlador do Selenium
         driver.get(url_demo)
         time.sleep(2)
-
         driver.find_element(By.CSS_SELECTOR, '[placeholder="Username"]').send_keys('Admin')
         driver.find_element(By.CSS_SELECTOR, '[placeholder="Password"]').send_keys('admin123')
         driver.find_element(By.CSS_SELECTOR,'#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button').click()
