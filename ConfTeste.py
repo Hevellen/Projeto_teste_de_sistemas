052fe17a387bb70169052cc30867d2e55b73569a
jonatasfarias <jotaflight@gmail.com>
2024-09-24 22:27:31 +0200

    testeok

--- ConfTeste.py
+++ ConfTeste.py
@@ -2,21 +2,23 @@ import pytest
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+url_demo = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
 class Test1:
         @pytest.fixture
-        def open_browser(self):
-                url_demo = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-                self.driver=webdriver.Chrome()
-                self.driver.get(url_demo)
+        def open_browser():
+                driver=webdriver.Chrome()
+                driver.get(url_demo)
+                yield driver
                 time.sleep(2)
-        def test_login_button(self,open_browser):
-                self.driver.find_element(By.CSS_SELECTOR, '[placeholder="Username"]').send_keys('Admin')
-                self.driver.find_element(By.CSS_SELECTOR, '[placeholder="Password"]').send_keys('admin123')
-                self.driver.find_element(By.CSS_SELECTOR,'#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button').click()
-                time.sleep(5)
-                assert self.driver.current_url=='https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index',"URL INválida"
-                self.driver.quit()
+        def test_login_button(open_browser):
+                driver=open_browser
+                driver.find_element(By.CSS_SELECTOR, '[placeholder="Username"]').send_keys('Admin')
+                driver.find_element(By.CSS_SELECTOR, '[placeholder="Password"]').send_keys('admin123')
+                driver.find_element(By.CSS_SELECTOR,'#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button').click()
+                assert driver.current_url=='https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index',"URL INválida"
+                driver.quit()
                 #consegui fazer o LOGIN
+
 
 
 
