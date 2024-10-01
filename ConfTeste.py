7d34ef819dc71d3350f7870343217429041f6dc5
jonatasfarias <jotaflight@gmail.com>
2024-10-01 21:52:22 +0200

    testeok

--- ConfTeste.py
+++ ConfTeste.py
@@ -6,20 +6,20 @@ url_demo = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
 
 @pytest.fixture
 def test_open_browser():
-                driver=webdriver.Chrome()
-                driver.get(url_demo)
-                yield driver
-                time.sleep(2)
-                driver.quit()
+    driver = webdriver.Chrome()
+    driver.get(url_demo)
+    yield driver
+    time.sleep(2)
+    driver.quit()
 @pytest.fixture
 def test_login_button(test_open_browser):
-                driver=test_open_browser
-                driver.find_element(By.CSS_SELECTOR, '[placeholder="Username"]').send_keys('Admin')
-                driver.find_element(By.CSS_SELECTOR, '[placeholder="Password"]').send_keys('admin123')
-                driver.find_element(By.CSS_SELECTOR,'#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button').click()
-                assert driver.current_url=='https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index',"URL INválida"
-                yield driver
-                #consegui fazer o LOGIN
+    driver = test_open_browser
+    driver.find_element(By.CSS_SELECTOR, '[placeholder="Username"]').send_keys('Admin')
+    driver.find_element(By.CSS_SELECTOR, '[placeholder="Password"]').send_keys('admin123')
+    driver.find_element(By.CSS_SELECTOR,'#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button').click()
+    assert driver.current_url=='https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index',"URL INválida"
+    yield driver
+    #consegui fazer o LOGIN
 
 
 
--- tests/CT04.py
+++ tests/CT04.py
@@ -1,11 +1,11 @@
-import pytest
 from selenium.webdriver.common.by import By
 url_demo = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
 
 class Test4:
-    def test_create_user(self, test_open_browser):
-        driver=test_open_browser
+    def test_create_user(self, test_login_button):
+        driver = test_login_button
         driver.findelement((By.CLASS_NAME,'class="oxd-icon bi-list oxd-topbar-header-hamburger"')).click()
+
         #driver.findelement(By.CLASS_NAME,'oxd-text oxd-text--span oxd-main-menu-item--name')).click()
         #driver.findelement(By.CSS_SELECTOR,'[type="button"]').click()
 
