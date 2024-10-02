4b26a8729d799a4817b12443634f88c584800d72
jonatasfarias <jotaflight@gmail.com>
2024-10-02 13:57:22 +0200

    testeok

--- tests/CT04.py
+++ tests/CT04.py
@@ -4,7 +4,7 @@ url_demo = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
 class Test4:
     def test_create_user(self, test_login_button):
         driver = test_login_button
-        driver.findelement((By.CLASS_NAME,'class="oxd-icon bi-list oxd-topbar-header-hamburger"')).click()
+        #driver.findelement((By.CLASS_NAME,'class="oxd-icon bi-list oxd-topbar-header-hamburger"')).click()
 
         #driver.findelement(By.CLASS_NAME,'oxd-text oxd-text--span oxd-main-menu-item--name')).click()
         #driver.findelement(By.CSS_SELECTOR,'[type="button"]').click()
