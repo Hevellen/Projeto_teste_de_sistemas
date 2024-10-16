7e23ac3974150d0bce74ab156432daefc3c9ee8b
h.amorim.bezerra <hevellen97kelly@gmail.com>
2024-10-15 22:41:59 -0300

    commit2

--- test/test_CT07.py
+++ test/test_CT07.py
@@ -1,4 +1,7 @@
+from telnetlib import EC
+
 from pages.Recruitment import test_Recruitment
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class Test_07:
@@ -8,4 +11,17 @@ class Test_07:
         assert login_p.test_is_url_login(), 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
 
 
+def access_recruitment(self, Recruitment):
+    WebDriverWait(self.driver, 10).until(
+        EC.visiliby_of_element_located(self.list_of_modules)
 
+    )
+
+
+    test_Recruitment = self.driver.find_elements(self.modulr_item)
+    for module in recruitment:
+        if module.text == test_Recruitment:
+           WebDriverWait(self.driver, 5).until(
+               EC.element_to_be_clickable(recruitment)
+           ).click()
+           break
